ce6ffcd6f247bbbf8e16f9a4b796739046aed021
Adenilson-Developers <adenilson.developers@gmail.com>
2022-10-14 23:09:13 -0300

    array

---
+++ 001-iniciando-array/iniciando-array.js
@@ -0,0 +1,17 @@
+let dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+// Acessando tamanho do array do array
+//console.log('--->', dayOfWeek.length);
+
+/* acessando elementos e fazendo uma interação
+ - vamos supor que queremos exibir todos os elementos do dayOfWeek,
+   para isso precisamso percorrer oarray com um laço e exibir os elementos 
+
+*/
+
+for(let i = 0; i < dayOfWeek.length; i++){
+    console.log(dayOfWeek[i]);
+}
+
+
+
