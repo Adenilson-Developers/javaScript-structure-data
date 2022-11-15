67ce347f01dcf9937fe9d3cda3ff9fdea10ccfb4
Adenilson-Developers <adenilson.developers@gmail.com>
2022-11-14 22:55:00 -0300

    adicionando classNode

---
+++ arvore_binaria_search/models/nodeClass.js
@@ -0,0 +1,9 @@
+
+/* Essa class representara cada nó de nossa arvore binaria de busca  */
+export class classNode {
+    constructor(key){
+        this.key = key;
+        this.left = null;
+        this.right = null;
+    }
+}
