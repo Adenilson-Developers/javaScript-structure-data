875c644456d0d6eb6300a8ecb775008f5bc78b8f
Adenilson-Developers <adenilson.developers@gmail.com>
2022-11-14 22:56:14 -0300

    adicionando função de comparação

---
+++ arvore_binaria_search/util/util.js
@@ -0,0 +1,10 @@
+export function defaultCompare(a,b){
+    console.log('cosole aqui -->',a)
+    return a === b
+    
+};
+
+export function Compare(a,b){
+    console.log('cosole aqui',a)
+    return a === b
+};
