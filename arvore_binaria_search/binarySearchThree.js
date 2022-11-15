5cf4ac034366e6db3da8689fbf67329c28a355ee
Adenilson-Developers <adenilson.developers@gmail.com>
2022-11-14 22:59:57 -0300

    adicionando um no na árvore

---
+++ arvore_binaria_search/binarySearchThree.js
@@ -0,0 +1,34 @@
+import { Compare, defaultCompare } from './util/util.js';
+import { classNode } from './models/classNode.js';
+
+export default class binarySeachTree {
+    constructor(compareFn = defaultCompare){
+        this.compareFn = compareFn;
+        this.root = null;
+    }
+    // inserir uma nova key em uma arvore 
+    insert(key){
+        if(this.root == null){
+            this.root = new classNode(key);
+
+        }else{
+            this.insertNode(this.root, key);
+        }
+    }
+
+    insertNode(node, key){
+        if(this.compareFn(key, node.key) === Compare.LESS_THAN){
+            if(node.left == null){
+                node.left = new classNode(key);
+            }else {
+                this.insertNode(node.left, key);
+            }
+        }else {
+            if(node.right == null){
+                node.right = new classNode(key);
+            }else {
+                this.insertNode(node.right, key);
+            }
+        }
+    }
+}
